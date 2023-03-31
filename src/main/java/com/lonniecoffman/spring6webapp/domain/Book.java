582d6ce950e446f29fc73092c338ef77603edb49
Lonnie Coffman <lonniecoffman@icloud.com>
2023-03-31 13:54:44 -0500

    equals and hashcode

--- src/main/java/com/lonniecoffman/spring6webapp/domain/Book.java
+++ src/main/java/com/lonniecoffman/spring6webapp/domain/Book.java
@@ -2,6 +2,8 @@ package com.lonniecoffman.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 public class Book {
 
@@ -46,5 +48,28 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+
+        return getId() != null ? getId().equals(book.getId()) : book.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId().hashCode() : 0;
     }
 }
