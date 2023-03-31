d5306c6fbdc269621974c850711c2d96752cf0d1
Lonnie Coffman <lonniecoffman@icloud.com>
2023-03-31 14:42:39 -0500

    bootstrap data

--- src/main/java/com/lonniecoffman/spring6webapp/repositories/BookRepository.java
+++ src/main/java/com/lonniecoffman/spring6webapp/repositories/BookRepository.java
@@ -3,5 +3,4 @@ package com.lonniecoffman.spring6webapp.repositories;
 import com.lonniecoffman.spring6webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
+public interface BookRepository extends CrudRepository<Book, Long> { }
